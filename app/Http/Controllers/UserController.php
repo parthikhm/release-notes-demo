bf45ce985a8f5fa240003141bc92338634fe089b
parthikhm <parthikmodi43@gmail.com>
2025-04-29 12:56:54 +0530

    ATTEMPT 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        // Handle the request to show the index page
+        return view('index');
+    }
 }
