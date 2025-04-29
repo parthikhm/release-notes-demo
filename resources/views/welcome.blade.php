12f089ea93833de993e1c74f103ed6e2350bdad6
parthikhm <parthikmodi43@gmail.com>
2025-04-29 12:16:09 +0530

    welcome blade file in add model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,30 +4,51 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Laravel Livewire Modal</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    @livewireStyles
 </head>
 
 <body>
     <div class="container mt-5">
-        <!-- Button to trigger modal for form -->
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#liveModal">
-            Launch Form modal
+        <!-- Button to trigger Form modal -->
+        <button type="button" class="btn btn-primary" wire:click="$emit('openModal', 'form')" data-bs-toggle="modal"
+            data-bs-target="#liveModal">
+            Launch Form Modal
         </button>
 
-        <!-- Button to trigger modal for table -->
-        {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#liveModal">
-            Launch Table modal
-        </button> --}}
+        <!-- Button to trigger Table modal -->
+        <button type="button" class="btn btn-primary" wire:click="$emit('openModal', 'table')" data-bs-toggle="modal"
+            data-bs-target="#liveModal">
+            Launch Table Modal
+        </button>
+
+        <div class="modal fade" id="liveModal" tabindex="-1" aria-labelledby="liveModalLabel" aria-hidden="true"
+            wire:ignore.self>
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="liveModalLabel">Livewire Modal</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        @livewire('dynamic-modal-body')
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <!-- Livewire Modal Component -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
     </script>
+
+    @livewireScripts
 </body>
 
 </html>
